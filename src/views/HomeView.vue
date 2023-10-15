788eed0e54801c8b67ea0624b53f90d101fa257b
Hazenhower-IT <alessio.aquilanti@outlook.com>
2023-10-15 14:40:25 +0200

    test-push

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Deployato con successo!</h1>
+    <TheWelcome />
+  </main>
+</template>
